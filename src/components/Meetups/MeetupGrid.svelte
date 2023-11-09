ec56d21e66599abff9f2ebcea0661d1b7dbdc9d1
Adnan <adbak.dev@gmail.com>
2023-11-09 22:25:55 +0100

    fc

---
+++ src/components/Meetups/MeetupGrid.svelte
@@ -0,0 +1,29 @@
+<script>
+	import MeetupItem from './MeetupItem.svelte';
+	export let meetups;
+</script>
+
+<section id="meetups">
+	{#each meetups as meetup}
+		<MeetupItem {...meetup} />
+	{/each}
+</section>
+
+<style>
+	#meetups {
+		margin-top: 5rem;
+	}
+
+	section {
+		width: 100%;
+		display: grid;
+		grid-template-columns: 1fr;
+		grid-gap: 1rem;
+	}
+
+	@media (min-width: 768px) {
+		section {
+			grid-template-columns: repeat(2, 1fr);
+		}
+	}
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+	import HomeComponent from './HomeComponent.svelte';
+	import './styles.css';
+</script>
+
+<link
+	rel="stylesheet"
+	href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"
+/>
+
+<HomeComponent />
